06acd2febca5d64a8380f6cf219e9a2a9f61d7cf
Николай Черниций <kolac300@gmail.com>
2023-03-26 12:50:43 +0300

    test commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
+import App from './components/App';
+import reportWebVitals from './vitals/reportWebVitals';
+import { sendToVercelAnalytics } from './vitals/vitals';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 reportWebVitals(sendToVercelAnalytics);
+
+
